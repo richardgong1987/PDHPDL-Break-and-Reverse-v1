49c89935f694e8b80561cc5efce6714162d54e92
H.Gong3 <h.gong3@liverpool.ac.uk>
2026-06-28 01:19:52 +0900

    days

--- PDHPDL Break and Reverse v1/Util/PdhpdlLineUtils.cs
+++ PDHPDL Break and Reverse v1/Util/PdhpdlLineUtils.cs
@@ -7,7 +7,7 @@ public class PdhpdlLineUtils
 {
     public static int GetDaysToDraw(Bars chartBars)
     {
-        if (chartBars.Count < 2) 
+        if (chartBars.Count < 2)
             return 2;
         DateTime chartStartDate = chartBars.OpenTimes[0].Date;
         DateTime chartEndDate = chartBars.OpenTimes[chartBars.Count - 1].Date;
